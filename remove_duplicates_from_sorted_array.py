62ff5908e6010a5dc416b9c5e26811693cb0ab34
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-09 09:20:44 +0700

    more efficient: remove duplicates from sorted array

--- remove_duplicates_from_sorted_array.py
+++ remove_duplicates_from_sorted_array.py
@@ -3,27 +3,14 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-
-        res = []
-        count = 0
-        for idx in range(len(nums) - 1):
-            if nums[idx] != nums[idx + 1]:
-                res.append(nums[idx])
-                count += 1
-
-        # handle last element of nums
-        # case when res has been filled
-        if len(nums) != 0 and len(res) != 0:
-            if nums[len(nums) - 1] != res[len(res) - 1]:
-                res.append(nums[len(nums) - 1])
-                count += 1
-
-        # case when res doesn't filled yet
-        if len(nums) != 0 and len(res) == 0:
-            res.append(nums[len(nums) - 1])
-            count += 1
-
-        for idx in range(count):
-            nums[idx] = res[idx]
-
-        return count
+        if len(nums) == 0:
+            return 0
+        else:
+            unique_element = nums[0]
+            idx = 0
+            for i in range(1, len(nums)):
+                if nums[i] != unique_element:
+                    idx += 1
+                    nums[idx] = nums[i]
+                    unique_element = nums[i]
+            return idx + 1
