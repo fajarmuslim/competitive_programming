f35edbcee1a89af7cf3e26e911e9b620c0e33417
fajar muslim <13517149@std.stei.itb.ac.id>
2021-07-10 08:40:54 +0700

    write pointer: remove element

--- remove-element.py
+++ remove-element.py
@@ -4,14 +4,23 @@ from typing import List
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
 
-        res = []
-        count = 0
-        for item in nums:
-            if item != val:
-                res.append(item)
-                count += 1
+        # res = []
+        # count = 0
+        # for item in nums:
+        #     if item != val:
+        #         res.append(item)
+        #         count += 1
 
-        for idx in range(count):
-            nums[idx] = res[idx]
+        # for idx in range(count):
+        #     nums[idx] = res[idx]
 
-        return count
+        # return count
+
+        # efficient way
+        write_pointer = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[write_pointer] = nums[i]
+                write_pointer += 1
+
+        return write_pointer
